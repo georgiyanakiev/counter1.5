4c3f8b9cf99611749d11ec7b0efecbd26b1ac2fc
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-06-22 18:22:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    clicks:0,
+  }
+
+  Plus = () => { 
+    this.setState({clicks:this.state.clicks +1 });
+  }
+  Minus = () => { 
+    this.setState({clicks:this.state.clicks -1 });
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="displaycount"></div>
+        <h1 className="Counter">{this.state.clicks}</h1>
+        <button onClick={this.Plus}>Plus</button>
+        <button onClick={this.Minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
